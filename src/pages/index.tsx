874edeaf6ebb11f1fbc4586d0bf5885bd1fbfc23
taku1115 <59477643+taku1115@users.noreply.github.com>
2021-11-04 20:25:24 +0900

    firebase setting

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,21 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import { db } from "src/firebase";
+import { collection, getDocs } from "firebase/firestore";
+import { useEffect } from "react";
 
 const Home: NextPage = () => {
+  const getUsers = async () => {
+    const querySnapshot = await getDocs(collection(db, "users"));
+    querySnapshot.forEach((doc) => {
+      console.log(`${doc.id} => ${doc.data().firstName}`);
+    });
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
   return (
     <div>
       <Head>
