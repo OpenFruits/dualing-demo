371a1c469927334c4fe5ba3b760137e746ccc6e0
taku1115 <59477643+taku1115@users.noreply.github.com>
2021-11-12 17:03:58 +0900

    setting tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,18 +3,25 @@ const colors = require("tailwindcss/colors");
 module.exports = {
   mode: "jit",
   purge: ["./src/**/*.{ts,tsx}"],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: false, // 'media' or 'class'
   theme: {
     extend: {
       colors: {
-        "light-blue": colors.sky,
+        sky: colors.sky,
         cyan: colors.cyan,
         theme: "#9DD6E5",
+        "theme-light": "#CEEBF2",
+        "theme-dark": "#424242",
+      },
+      backgroundImage: {
+        "first-view": "url('/firstview_bg.png')",
+        persons: "url('/persons.png')",
       },
     },
+    boxShadow: {
+      md: "0 4px 6px -1px rgba(0, 0, 0, 0.4), 0 2px 4px -1px rgba(0, 0, 0, 0.06)",
+    },
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+  variants: { extend: {} },
+  plugins: [require("@tailwindcss/typography")],
 };
