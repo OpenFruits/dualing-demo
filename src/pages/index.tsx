288d9e3a1c54e68bbf1233af2240e9c50a1e398e
taku1115 <59477643+taku1115@users.noreply.github.com>
2021-11-13 18:13:41 +0900

    ⚰️

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,5 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import { db } from "src/firebase";
-import { collection, doc, getDocs, getDoc, setDoc } from "firebase/firestore";
-import { useEffect } from "react";
 import { Header } from "src/components/LP/Header";
 import { FirstView } from "src/components/LP/FirstView";
 import { Whatis } from "src/components/LP/Whatis";
@@ -12,43 +9,6 @@ import { FAQ } from "src/components/LP/FAQ";
 import { Footer } from "src/components/LP/Footer";
 
 const Home: NextPage = () => {
-  // 書き込み
-  const setUser = async () => {
-    const usersRef = collection(db, "users");
-    await setDoc(doc(usersRef), {
-      firstName: "荒川",
-      lastName: "テスト君",
-    });
-  };
-
-  // 一覧取得
-  const getUsers = async () => {
-    const querySnapshot = await getDocs(collection(db, "users"));
-    querySnapshot.forEach((doc) => {
-      console.log(`${doc.id} => ${doc.data().firstName}`);
-    });
-  };
-
-  // 個別取得
-  const getUser = async () => {
-    const docRef = doc(db, "users", "012345");
-    const docSnap = await getDoc(docRef);
-
-    if (docSnap.exists()) {
-      console.log(docSnap.id);
-      console.log(docSnap.data());
-      console.log(docSnap.data().firstName);
-    } else {
-      console.log("No such document!");
-    }
-  };
-
-  useEffect(() => {
-    // setUser();
-    // getUsers();
-    // getUser();
-  }, []);
-
   return (
     <>
       <Head>
